0673a43768862182591b45b529063c575ea54fd3
MrMcHir <102164593+MrMcHir@users.noreply.github.com>
2022-09-26 20:41:07 +0600

    Update game-finder.lua

--- game-finder.lua
+++ game-finder.lua
@@ -1,43 +1,16 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
-
-function RUN_FUNCTION(name, extension, configOpt)
-    local settings = configOpt;
-    local PATH_URL = settings.URL or nil;
-    local GAME_NAME = "";
-
-    local success = pcall(function()
-        game:HttpGet(PATH_URL)
-    end);
-
-    local DecodedTable;
-    if (success) then
-        if extension == "lua" then
-            DecodedTable = loadstring(game:HttpGet(PATH_URL))()
-        elseif extension == "json" then
-            DecodedTable = game:GetService("HttpService"):JSONDecode(game:HttpGet(PATH_URL))
-        end
-
-        settings.Executable = ""
-        for i, v in pairs(DecodedTable) do
-            if (game.PlaceId == tonumber(i) and v.name and v.Working) then
-                local connection = pcall(function()
-                    settings.Executable = game:HttpGet(v.Script)
-                end);
-
-                GAME_NAME = v.name
-            end
-        end
-    end
-
-    lib:MakeNotification({
-        Name = "Game Found!",
-        Content = "Game: " .. GAME_NAME .. "!",
-        Icon = "rbxassetid://11044015191",
-        Time = 5
-    })
-    wait(.5)
-
-    loadstring(settings.Executable)()
+lib:MakeNotification({
+    Name = "Game Found!",
+    Content = "Game: " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "!",
+    Icon = "rbxassetid://11044015191",
+    Time = 5
+})
+if game.PlaceId == 648362523 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Breaking%20Point.lua"))()
+elseif game.PlaceId == 192800 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Work%20At%20A%20Pizza%20Place.lua"))()
+elseif game.PlaceId == 1537690962 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Bss.lua"))()
+elseif game.PlaceId == 621129760 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Kat.lua"))()
 end
-
-return RUN_FUNCTION;
