a549793bacdc10a7f4dd5b6d55b5e4914229c403
MrMcHir <102164593+MrMcHir@users.noreply.github.com>
2022-09-30 10:27:36 +0600

    Update game-finder.lua

--- game-finder.lua
+++ game-finder.lua
@@ -1,10 +1,14 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
-lib:MakeNotification({
-    Name = "Game Found!",
-    Content = "Game: " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "!",
-    Icon = "rbxassetid://11044015191",
-    Time = 5
-})
+spawn(function()
+	function loadgame()
+        lib:MakeNotification({
+            Name = "Game Found!",
+            Content = "Game: " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "!",
+            Icon = "rbxassetid://11044015191",
+            Time = 5
+        })
+    end
+end)
 if game.PlaceId == 648362523 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Breaking%20Point.lua"))()
 elseif game.PlaceId == 192800 then
@@ -13,4 +17,11 @@ elseif game.PlaceId == 1537690962 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Bss.lua"))()
 elseif game.PlaceId == 621129760 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Kat.lua"))()
+else
+    lib:MakeNotification({
+        Name = "Game Not Found!",
+        Content = "Game: Not Supported",
+        Icon = "rbxassetid://11044015191",
+        Time = 5
+    })
 end
