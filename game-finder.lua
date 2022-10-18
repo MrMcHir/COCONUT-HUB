adbd780f43ce10c2e684f54550c1c608832e21e3
MrMcHir <102164593+MrMcHir@users.noreply.github.com>
2022-10-18 23:05:22 +0600

    Update game-finder.lua

--- game-finder.lua
+++ game-finder.lua
@@ -10,25 +10,18 @@ spawn(function()
     end
 end)
 if game.PlaceId == 648362523 then
-    loadgame()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Breaking%20Point.lua"))()
 elseif game.PlaceId == 192800 then
-    loadgame()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Work%20At%20A%20Pizza%20Place.lua"))()
 elseif game.PlaceId == 1537690962 then
-    loadgame()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Bss.lua"))()
 elseif game.PlaceId == 621129760 then
-    loadgame()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Kat.lua"))()
 elseif game.PlaceId == 537413528 then
-    loadgame()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/babft.lua"))()
 elseif game.PlaceId == 8726743209 then
-    loadgame()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/RefineryCaves.lua"))()
 elseif game.PlaceId == 9872472334 then
-    loadgame()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMcHir/COCONUT-HUB/main/Games/Evade.lua"))()
 else
 	hahaha:MakeNotification({
